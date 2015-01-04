f06e7c888021971ca8670b122148000f23d0b284
Allen Van <all3nvan@gmail.com>
2015-01-04 08:53:05 -0500

    Added sleep to prevent rate limiting Riot API

--- app/jobs/update_job.rb
+++ app/jobs/update_job.rb
@@ -7,22 +7,34 @@ class UpdateJob < ActiveJob::Base
   API_KEY = "f7e80d6f-340b-450a-b2aa-12ba2e8e6da8"
 
   def perform(player)
+    sleep(11)
+    numberOfRequests = 0
+
     response = JSON.parse(open("#{RIOT_API_URL}v1.3/game/by-summoner/#{player.summonerId}/recent?api_key=#{API_KEY}").read)
     games = response["games"]
+    numberOfRequests += 1
 
     games.each do |game|
-    	if inhouse?(game) && !in_database?(game)
-        inhouseGame = JSON.parse(open("#{RIOT_API_URL}v2.2/match/#{game["gameId"]}").read)
-    		player.game_stats.create(game_id: inhouseGame["gameId"])
+    	if ( inhouse?(game) && !in_database?(game) )
+        inhouseGame = JSON.parse(open("#{RIOT_API_URL}v2.2/match/#{game["gameId"]}?api_key=#{API_KEY}").read)
+    		player.game_stats.create(game_id: inhouseGame["matchId"])
+
+        numberOfRequests += 1
     	end
+
+      if numberOfRequests == 10
+        sleep(11)
+        numberOfRequests = 0
+      end
     end
   end
 
   def inhouse?(game)
-    (game["gameType"] == "CUSTOM_GAME") && (game["fellowPlayers"].count == 10)
+    (game["gameType"] == "CUSTOM_GAME") && (game["fellowPlayers"].count == 9)
   end
 
   def in_database?(game)
     Game.exists?(game_id: game["gameId"])
+    # HAVENT PUT GAME IDS INTO DATABASE YET SO THIS IS ALWAYS FALSE
   end
 end
