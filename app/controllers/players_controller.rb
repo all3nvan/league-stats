6eb71b6ac7b821b960328b1c753b70f582066b36
Allen Van <all3nvan@gmail.com>
2015-01-21 23:41:57 -0500

    Added minimum games option

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,8 +2,8 @@ class PlayersController < ApplicationController
 
 	def index
 		@players = Player.all
-		#@minimum = params[:minimum].nil? ? 5 : params[:minimum]
-		@sorted_winrates = get_winrates(@players)#, @minimum)
+		@min = params[:minimum].nil? ? 5 : params[:minimum].to_i
+		@sorted_winrates = get_winrates(@players, @min)
 	end
 
 	def show
@@ -11,8 +11,8 @@ class PlayersController < ApplicationController
 		@sorted_stats = @player.get_stats
 	end
 
-	def get_winrates(players)#, min)
-		winrates = players.map do |player|
+	def get_winrates(players, min)
+		winrates = players.select{ |player| player.game_stats.count >= min }.map do |player|
 			{"object" => player,
 			 "wins" => player.game_stats.where("win = ?", true).count,
 			 "losses" => player.game_stats.where("win = ?", false).count,
